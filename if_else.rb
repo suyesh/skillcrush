8d85f30ad8a02227eefe98f5f49b32926c58e047
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-31 20:58:35 -0400

    some stuff

---
+++ if_else.rb
@@ -0,0 +1,32 @@
+if 1 + 1 == 2
+  puts "1 and 1 does indeed equal 2"
+end
+
+my_name = 'Skillcrush'
+
+if my_name == 'Skillcrush'
+  puts "Heloooooo, Skillcrush"
+end
+
+if my_name == 'Skillcrush'
+  puts "Hellooooo, Skillcrush"
+else
+  puts "OOps, I thought your name was skillcrush. Sorry about that, #{my_name}"
+end
+
+fav_color = 'pink'
+if (fav_color == 'red')
+  puts "Red like fire!"
+elsif (fav_color == 'orange')
+  puts "Orange like, well... an orange."
+elsif (fav_color == 'yellow')
+  puts "Yellow daffodils are pretty in the spring!"
+elsif (fav_color == 'green')
+  puts "Have you been tot he Emerald City in Oz?"
+elsif (fav_color == 'blue')
+  puts "Blue like the sky!"
+elsif (fav_color == 'purple')
+  puts "Purple plums are the tastiest."
+else
+  puts "Hmm, well I don't know what that color is!"
+end
